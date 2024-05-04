33c11dc85554ff50e982358c10982bde41c8b295
Patrick Maes <patrickmaes@mines.edu>
2024-05-03 21:56:15 -0600

    Added layout to the Layout.js file. Still needs images and text.

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -3,45 +3,37 @@ import './App.css';
 import './styles.css'; // Import the new CSS file
 import React, { useState } from 'react';
 import { useLoaderData, Link } from 'react-router-dom';
-// We treat `Layout` as the layout of our application.
-// The `<Outlet />` delegates renders to the matching child route, if one exists.
+
 function Layout() {
   const [todoDescription, setTodoDescription] = useState('');
   const [completed, setCompleted] = useState(false);
-  const [selectedMethod, setSelectedMethod] = useState('GET'); // State for selected HTTP method
+  const [selectedMethod, setSelectedMethod] = useState('GET');
   const [id, setId] = useState('');
-  // Event handler for selecting HTTP method
+
   const handleMethodChange = (e) => {
     setSelectedMethod(e.target.value);
   };
 
-  // Event handler for updating TODO description
   const handleDescriptionChange = (e) => {
     setTodoDescription(e.target.value);
   };
 
-  // Event handler for updating completed status
   const handleCompletedChange = () => {
     setCompleted(!completed);
   };
 
-  // Event handler for updating ID
   const handleIdChange = (e) => {
     setId(e.target.value);
   };
+
   const handleSubmit = () => {
-    // Reset fields after submission (optional)
     if (selectedMethod === 'GET') {
-      // Perform the GET request to fetch data
       fetch(`http://localhost:3001/${id}`)
         .then(response => response.json())
         .then(data => {
-          // Handle the data fetched from the server
           console.log('Data fetched:', data);
-          // You can update state or perform any other action here
         })
         .catch(error => {
-          // Handle any errors that occur during the request
           console.error('Error fetching data:', error);
         });
     }
@@ -49,11 +41,21 @@ function Layout() {
     setCompleted(false);
     setId('');
   };
+
   return (
     <>
       <h1>Ore Market</h1>
-      
-      <Outlet />
+      <div className="layout-container">
+        <div className="column">
+          {/* Content for the first column */}
+          <h2>Buy and Sell Items!</h2>
+
+        </div>
+        <div className="column">
+          {/* Content for the second column */}
+          <h2>Make Connections and Friends...</h2>
+        </div>
+      </div>
     </>
   );
 }
